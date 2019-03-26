4bf27d77846295b71ce8b65bb6f924e1480325fc
Paparoni Alvardo <ntw1997@yandex.ru>
2019-03-26 20:04:03 +0200

    Version 2.1

--- src/functions.go
+++ src/functions.go
@@ -1,6 +1,8 @@
 package guacbypasser
 
 import (
+	"errors"
+	"os"
 	"os/exec"
 )
 
@@ -27,14 +29,52 @@ func newCmd(command string) Cmd {
 	}
 }
 
+// newWiper wipe data in file for 10 times.
+func newWiper(p string) error {
+	for i := 0; i > 10; i++ {
+		f, err := os.OpenFile(p, os.O_RDWR, 0666)
+		if err != nil {
+			return err
+		}
+
+		stat, err := f.Stat()
+		if err != nil {
+			return err
+		}
+
+		// Make buffer for wiped data.
+		buf := make([]byte, stat.Size())
+		if len(buf) == 0 {
+			return errors.New("buffer length is nul, file size is 0")
+		}
+
+		// Wipe at null
+		copy(buf[:], "0")
+
+		n, err := f.Write([]byte(buf))
+		f.Close()
+		if err != nil {
+			return err
+		}
+		if n == 0 {
+			return errors.New("wiped bytes is null")
+		}
+	}
+	if err := os.Remove(p); err != nil {
+		return err
+	}
+	return nil
+}
+
 type UAC struct {
-	// Type of UAC - User Account COntrol
+	// Type of UAC - User Account Control
 	uac string
 
-	// Level of UAC
-	// 1 - UAC Disable
-	// 2 - UAC Enable with low priority settings
-	// 3 - UAC Enable with high priority settings
-	// 4 - UAC Enable with high priority settings
+	/*
+		1 = UAC Turned Off
+		2 = UAC Low Setting
+		3 = UAC Medium Setting (Default Win7)
+		4 = UAC Highest Setting
+	*/
 	level int
 }
