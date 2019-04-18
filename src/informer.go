3c0feada5cfab692da6e35ba01d3bb1f1b70614d
Paparoni Alvardo <ntw1997@yandex.ru>
2019-04-18 14:16:42 +0300

    Add missing files

---
+++ src/informer.go
@@ -0,0 +1,17 @@
+package guacbypasser
+
+type Informer struct {
+	Name string
+	Desc string
+
+	Id int
+
+	Type   string
+	Module string
+
+	Fixed   bool
+	FixedIn string
+
+	Admin   bool
+	Payload bool
+}
