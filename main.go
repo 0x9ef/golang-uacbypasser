1e0db7594f3429050155c919b8056bf955a298c9
0x9ef <0x9ef@tutanota.com>
2022-03-27 21:20:48 +0300

    Corrected year of license

--- main.go
+++ main.go
@@ -1,4 +1,4 @@
-// Copyright (c) 2022 0x9ef. All rights reserved.
+// Copyright (c) 2019-2022 0x9ef. All rights reserved.
 // Use of this source code is governed by an MIT license
 // that can be found in the LICENSE file.
 package main
